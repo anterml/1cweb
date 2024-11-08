a48d157a3159d77386cd87f8c0e05608c46b7526
Anton <caldiary11@gmail.com>
2024-11-08 20:34:56 +0300

    remove whitespaces

--- src/lib/types/entity.ts
+++ src/lib/types/entity.ts
@@ -1,6 +1,9 @@
-	export type EntityKeys = "Справочники" | "Документы"
-  export type SubEntityKeys = "Реквизиты" | "Формы" | "Табличные части"
-  export type EntityEngKeys = "spravochniki" | "documenti"
-  export type Setting = 
-  | { name: "Строка", type: 'text', len: number }
-  | { name: "Число", type: 'number', len: number, precision: number, negative: boolean }
+export type EntityKeys = "Справочники" | "Документы"
+export type SubEntityKeys = "Реквизиты" | "Формы" | "Табличные части"
+export type EntityEngKeys = "spravochniki" | "documenti"
+
+export type EntityItem = {
+  id: string,
+  type: EntityEngKeys,
+  name: string,
+}
